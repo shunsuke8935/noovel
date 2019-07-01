32f05da281192dacc6d5b1887ce969ba4a656332
Shunsuke Goto <shunsuke8935@gmail.com>
2019-07-01 12:50:37 +0000

    create model and controller

---
+++ app/helpers/clip_writers_helper.rb
@@ -0,0 +1,2 @@
+module ClipWritersHelper
+end
---
+++ test/controllers/novels_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class NovelsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get novels_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get novels_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get novels_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get novels_edit_url
+    assert_response :success
+  end
+
+end
