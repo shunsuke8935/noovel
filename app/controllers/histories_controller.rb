8d688cc2d1a2691e986602007e91aedaf00bc48c
Shunsuke Goto <shunsuke8935@gmail.com>
2019-07-15 09:13:41 +0000

    book-history crate

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,6 +1,5 @@
 class HistoriesController < ApplicationController
   def index
-  	@novel = Novel.find_by(user_id: current_user.id)
-  	@history = History.find_by(user_id:current_user.id, novel_id:@novel)
+  	@history = History.where(user_id: current_user.id)
   end
 end
