d8062d2a9a802a9217f65cce7f91a7c6f7bb83ce
Shunsuke Goto <shunsuke8935@gmail.com>
2019-07-28 11:00:26 +0000

    layout fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	@novels = @user.novels
+  	@favorites = Favorite.where(novel_id: @novels)
   end
 
   def edit
